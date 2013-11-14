c60b844939c45207c9e137b3cd623341effb5e5f
a.l.e <ale@ideale.ch>
2013-11-14 11:01:29 +0100

    rescue the ui_iv_iu project files

---
+++ ui_iv_iu/README.md
@@ -0,0 +1,1 @@
+This was the wiki created in 2010 for the UI project around scribus.
---
+++ ui_iv_iu/pages/Howto.C
@@ -0,0 +1,8 @@
+(:title C C++ Qt:)
+
+* [[http://claymore.engineer.gvsu.edu/~steriana/226/C.CheatSheet.pdf|The C Cheat Sheet, An Introduction to Programming in C]] (pdf)
+* [[http://tuxtraining.com/files/c1.pdf|C Cheat Sheet]]
+* [[http://refcards.com/docs/silvermanj/ansi-c/ansi-c-refcard-a4.pdf|C Reference Card (ANSI)]]
+* [[http://www.cheat-sheets.org/saved-copy/c_cheat_sheet.pdf|C/C++ Cheat Sheet]]
+* [[http://qt.nokia.com/downloads]]
+* [[http://doc.qt.nokia.com/4.6/]]
